0de95115763bafeed1e05ccc26c17c2c09696798
serubi <serubi@live.com>
2018-09-10 12:14:36 +0200

    Tilføjet Studerende class med instance fields, backing fields og constructor

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        private string _navn;
+        private int _fødselsmåned;
+        private int _fødselsdag;
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            _navn = navn;
+            _fødselsmåned = fødselsmåned;
+            _fødselsdag = fødselsdag;
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public int Fødselsmåned
+        {
+            get { return _fødselsmåned; }
+            set { _fødselsmåned = value; }
+        }
+
+        public int Fødselsdag
+        {
+            get { return _fødselsdag; }
+            set { _fødselsdag = value; }
+        }
+    }
+    }
+}
