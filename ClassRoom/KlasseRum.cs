6e604863e1dd87193e70be9f13b9e8b6f17452f5
serubi <serubi@live.com>
2018-09-10 12:20:39 +0200

    Tilføjet KlasseRum class med properties og constructor

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        private string _klasseNavn;
+        private List<Studerende> _klasseliste;
+        private DateTime _semesterstart;
+
+        public KlasseRum()
+        {
+            
+        }
+
+        public string KlasseNavn
+        {
+            get { return _klasseNavn; }
+            set { _klasseNavn = value; }
+        }
+
+        public List<Studerende> Klasseliste
+        {
+            get { return _klasseliste; }
+            set { _klasseliste = value; }
+        }
+
+        public DateTime SemesterStart
+        {
+            get { return _semesterstart; }
+            set { _semesterstart = value; }
+        }
+    }
+}
